b20962ad82d3d4f2ee7c3c6180a3f91ffbdc0be4
Forbes Lindesay <forbes@lindesay.co.uk>
2019-01-31 16:56:58 +0000

    resolves #1
    resolves #2

--- src/use-force-update.ts
+++ src/use-force-update.ts
@@ -9,12 +9,11 @@ interface VoidFunctionCreator {
   (): VoidFunction;
 }
 
-const max: number = 9007199254740990; // Number.MAX_SAFE_INTEGER - 1;
-
+const toggle = (state: boolean) => !state;
 const useForceUpdate: VoidFunctionCreator = (): VoidFunction => {
-  const [ , setState ] = useState(0);
+  const [ , setState ] = useState(false);
   const forceUpdate: VoidFunction = (): void => {
-    setState((state: number) => (state + 1) % max);
+    setState(toggle);
   };
   return forceUpdate;
 };
