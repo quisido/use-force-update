f3f5aa1a1e489ed25209da1f37625755edb37be1
Charles Stover <github@charlesstover.com>
2019-06-23 11:50:48 -0700

    resolves #9

--- src/use-force-update.ts
+++ src/use-force-update.ts
@@ -1,11 +1,17 @@
 import { useCallback, useReducer } from 'react';
 
+type EmptyObject = {};
+
 type VoidFunction = () => void;
 
-const reducer = (state: boolean, _action: null): boolean => !state;
+// Returning a new object reference guarantees that a before-and-after
+//   equivalence check will always be false, resulting in a re-render, even
+//   when multiple calls to forceUpdate are batched.
+const reducer = (): EmptyObject => Object.create(null);
 
 const useForceUpdate = (): VoidFunction => {
-  const [ , dispatch] = useReducer<boolean, null>(reducer, true);
+  const [ , dispatch] =
+    useReducer<EmptyObject, null>(reducer, Object.create(null));
 
   // Turn dispatch(required_parameter) into dispatch().
   const memoizedDispatch = useCallback(
